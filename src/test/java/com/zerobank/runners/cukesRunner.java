b7fbb12b40ac365d1d4e71ca459db99e4c1e6ac9
MDogan <mdoganusa@gmail.com>
2021-07-28 18:34:57 -0400

    Initial commit

---
+++ src/test/java/com/zerobank/runners/cukesRunner.java
@@ -0,0 +1,19 @@
+package com.zerobank.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/features/login.feature",
+        glue = "com/zerobank/step_definitions",
+        tags = "@wip"
+
+)
+
+
+public class cukesRunner {
+
+
+}
